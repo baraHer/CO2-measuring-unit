50cd11aaf2c77ba26f9f170ae6717481539372f6
Barbora Herynková <b.herynkova@gmail.com>
2025-01-03 17:19:37 +0100

    code fixes, api error catch and prevention

--- co2-measuring-unit/src/components/LocationForm.jsx
+++ co2-measuring-unit/src/components/LocationForm.jsx
@@ -17,20 +17,34 @@ const LocationForm = () => {
     const API_BASE_URL = 'http://api.weatherapi.com/v1'
     const API_KEY = '0e813ae11e09437e95934223250301'
 
+    useEffect(() => {
+        if (weatherData) {
+            console.log(weatherData);
+        }
+    }, [weatherData]);
+
     const getWeatherData = async (location) => {
+        try {
+            const apiUrlWeather = `${API_BASE_URL}/forecast.json?key=${API_KEY}&q=id:${location.id}&lang=cs`
+            const response = await fetch(apiUrlWeather)
+            const data = await response.json()
 
-        const apiUrlWeather = `${API_BASE_URL}/forecast.json?key=${API_KEY}&q=id:${location.id}&lang=cs`
-        const response = await fetch(apiUrlWeather)
-        const data = await response.json()
-
-        console.log(data)
-        setWeatherData(data)
+            setWeatherData(data)
+        }
+        catch (error) {
+            console.log(error)
+        }
     }
 
     const getLocation = async () => {
         const apiUrlLocation =`${API_BASE_URL}/search.json?key=${API_KEY}&q=${locationInput}&lang=cs`
-        const responseLocation = await fetch(apiUrlLocation)
-        return await responseLocation.json()
+        try {
+            const responseLocation = await fetch(apiUrlLocation)
+            return await responseLocation.json()
+        }
+        catch (error) {
+            console.error(error)
+        }
     }
 
     const submit = async (event) => {
@@ -46,14 +60,13 @@ const LocationForm = () => {
     const handleLocationInputChange = async (event) => {
         const newLocation = event.target.value;
         setLocationInput(newLocation);
-        if (newLocation.length > 1) {
+        if (newLocation.length > 2) {
             const possibleLocations = await getLocation()
-            let suggestedCities = []
-            for (let i = 0; i < possibleLocations.length && i < 3; i++) {
-                suggestedCities.push(possibleLocations[i])
-                setLocationAutofill(suggestedCities)
-                console.log(suggestedCities)
-            }
+            const suggestedCities = possibleLocations.slice(0, 3)
+            setLocationAutofill(suggestedCities)
+        }
+        else {
+            setLocationAutofill([])
         }
     }
 
@@ -71,12 +84,15 @@ const LocationForm = () => {
                 <div className='autofill-box'>
                     {
                         locationAutofill.map( (suggestedCity, index) => {
-                            const {name, region, country, id} = suggestedCity
+                            const {name, region, country} = suggestedCity
                             return <button type='button' onClick={ () => submitDirect(suggestedCity)} key={index}>{`${name}, ${region}, ${country}`}</button>
                         })
                     }
                 </div>
-                <button onClick={submit}>Odeslat</button>
+                <button className='submit-btn' onClick={submit} disabled={locationInput.length < 3}>
+                    {locationInput.length < 3 || 'Zobraz předpověď!'}
+                    {locationInput.length < 3 && 'Zadej alespoň 3 znaky!'}
+                </button>
             </form>
         </div>
     );
